abcdf07f1f59d2ab15e8bf527cb160083b1b480b
ValeriyNiz <98323131+ValeriyNiz@users.noreply.github.com>
2023-02-22 00:38:27 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 
-import AuthForm from './AuthForm/AuthForm';
+
 import Layout from './Layout/Layout';
 
 export const App = () => {
   return (
-    <AuthForm />
+    
     <Layout />    
   )};
 

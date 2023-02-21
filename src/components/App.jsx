6a2c03779e904f0040115de38b12ad79ca338cb3
ValeriyNiz <98323131+ValeriyNiz@users.noreply.github.com>
2023-02-22 00:28:48 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,18 +5,6 @@ import Layout from './Layout/Layout';
 export const App = () => {
   return (
     <AuthForm />
-    <Layout />
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101',
-    //   }}
-    // >
-    //   React homework template
-    // </div>
-  );
+    <Layout />    
+  )};
 

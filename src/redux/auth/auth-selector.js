6017982d952fc27613e030d1622396bfd8674fad
ValeriyNiz <98323131+ValeriyNiz@users.noreply.github.com>
2023-02-26 15:55:57 +0200

    Update auth-selector.js

--- src/redux/auth/auth-selector.js
+++ src/redux/auth/auth-selector.js
@@ -1,3 +1,4 @@
 export const getIsLoggedIn = state => state.auth.isLogin;
 export const getToken = state => state.auth.accessToken;
 export const getMessage = state => state.auth.message;
+export const getBalance = state => state.auth.user.balance;

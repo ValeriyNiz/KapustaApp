98db961e554e551355e8d4f5b79ba0f3fef1fc58
Valeriy Nizovtsev <nizucer123@gmail.com>
2023-02-26 17:34:37 +0200

    add navigate

--- src/components/ToTransaction/ToTransaction.jsx
+++ src/components/ToTransaction/ToTransaction.jsx
@@ -1,16 +1,25 @@
 import css from './ToTransaction.module.css';
 import Sprite from '../../images/sprite.svg';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+
 
 export const ToTransaction = () => {
+
+  const navigate = useNavigate();
+
+  const goToExpensesPage = () => {
+    navigate("MobileProductForm");
+  };
+
   return (
     <div className={css.linkFlex}>
-      <Link to="MobileProductForm">
+      <button onClick={goToExpensesPage}>
       <svg width="14" height="9">
         <use href={`${Sprite}#back-arrow`}></use>
       </svg>
       <p className={css.transaction}>To Transaction</p>
-      </Link>
+      </button>
     </div>
   );
 };

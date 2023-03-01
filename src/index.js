e0347e23dd4bbf70cd8f794261faebdd8de69420
ValeriyNiz <nizucer123@gmail.com>
2023-03-01 12:35:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <PersistGate loading={<div>...loading</div>} persistor={persistor}>
-      <BrowserRouter basename="KapustaApp">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </PersistGate>
